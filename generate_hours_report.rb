becd33e71e66fa393862ba5b13457af67968a9f5
gwmccubbin <gwmccubbin@gmail.com>
2014-11-25 14:24:43 -0600

    Adds script for generating hours report

---
+++ generate_hours_report.rb
@@ -0,0 +1,5 @@
+require './requirements.rb'
+
+# Gets newest file in directory
+newest_file_path = Dir.glob(File.join('./tasks', '*.*')).max { |a,b| File.ctime(a) <=> File.ctime(b) }
+HoursConverter.perform newest_file_path
