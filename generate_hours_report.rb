4006c0dc175374ed8073210f3a5be9c7d942565e
gwmccubbin <gwmccubbin@gmail.com>
2014-11-26 10:57:53 -0600

    Updates variable syntax in hours script

--- generate_hours_report.rb
+++ generate_hours_report.rb
@@ -1,5 +1,5 @@
 require './requirements'
 
 # Gets newest file in directory
-newest_file_path = Dir.glob(File.join('./tasks', '*.*')).max { |a,b| File.ctime(a) <=> File.ctime(b) }
-HoursConverter.perform newest_file_path
+newest_file = Dir.glob(File.join('./tasks', '*.*')).max { |a,b| File.ctime(a) <=> File.ctime(b) }
+HoursConverter.perform newest_file
